5a7eea2b58810ea615c29dfe850c5c6a0e4d4cb2
Robert Warren <2110981@wholefoods.com>
2021-10-17 19:47:00 -0500

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
+console.log("JS Loaded")
 var buttonEl = document.querySelector("#save-task"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler =function() {
+var createTaskHandler =function(event) {
+    event.preventDefault()
+    var userEntry = document.getElementById("task-name").value
+    var userTask=document.getElementsByName("task-type")[0].value
+    console.log(userTask,"Test")
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    listItemEl.textContent = "This is a new task." + userEntry + '- ' + userTask;
     tasksToDoEl.appendChild(listItemEl);
-}
+};
+buttonEl.addEventListener("click", createTaskHandler);
 
+ /*var createTaskHandler =function(event) {
+    event.preventDefault()
+    var userEntry=document.getElementById("task-type").value
+    console.log(userEntry, "Test")
+    var listItemEl =document.cre 
+}
+*/
 buttonEl.addEventListener("click", createTaskHandler);
 
 
